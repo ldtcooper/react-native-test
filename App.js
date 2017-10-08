c6f5cfc138f1f7eedc836fffe22b9b2b05ad4bab
Logan Cooper <ldtcoop@gmail.com>
2017-10-07 23:49:48 -0700

    Add state

--- App.js
+++ App.js
@@ -21,17 +21,32 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-export default class App extends Component<{}> {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {showText: false};
+    this.viewText = this.viewText.bind(this);
+  }
+
+  viewText() {
+    this.setState({showText: !this.state.showText });
+  }
+
   render() {
+    let display = this.state.showText ? "Borky Borky Borky, can't you see? Sometimes your words just hypontize me." : "";
     return (
       <View>
         <Text>
-          Borky Borky Borky, can't you see? Sometimes your words just hypontize me.
+          {display}
         </Text>
         <Image
-          style={{width: '100%', height: '100%'}}
+          style={{width: '100%', height: '80%'}}
           source={{uri: 'https://i.imgur.com/UwCfDrI.jpg'}}
         />
+      <Button
+        onPress={this.viewText}
+        title="Bork!"
+        color="#841584"/>
       </View>
     );
   }
